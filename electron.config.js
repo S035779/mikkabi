54f72b94630740124da9fa247a378a3aaa58b7ee
Mamoru Hashimoto <mamoru_hashimoto@hotmail.com>
2017-10-19 12:47:15 +0900

    modified modified:   electron.config*

--- electron.config.js
+++ electron.config.js
@@ -11,19 +11,20 @@ packager({
   , version: "1.7.9"
   , overwrite: true
   , asar: true
-  , ignore: "\.git|src|tmp|utils|dist|node_modules/(electron-packager|electron-built|\.bin)|\.girignore|npm-debug.log"
   , "app-version": package["version"]
   , "app-copyright": "Copyright (C) 2017 "
     + package["author"] + "."
   , "version-string": {
-    CompanyName: "Hashi Design.,Inc."
+    CompanyName: "HashiDesign.,Inc."
     , FileDescription: package["name"]
     , OriginalFilename: package["name"] + ".exe
     , "ProductName": package["name"]
     , InternalName: package["name"]
   }
+  , prune: true
+  , ignore: "\.git|\.gitignore|npm-debug.log|node_modules/(electron-packager|electron-prebuilt|\.bin)|public/(bundle\.js\.map|bundle\.css\.map)|src|utils|dist|tmp"
 }, function (err, appPaths) {
-  if (err) console.log(err);
-  console.log("Done: " + appPaths);
+  if(err) throw new Error(err);
+  console.log("Done: ", appPaths);
 });
 
